21ee4d4f56266f33106bcc4c117fdba61a31fb5b
lmkindermann <lmkindermann@gmail.com>
2026-04-08 16:49:58 -0300

    Updating portfolio to fully reflect my CV and freelance services providing. Added translation functionality through json file

--- README.md
+++ README.md
@@ -4,18 +4,20 @@ Link do repositório no GitHub: https://github.com/lmkindermann/portfolio_lucas_
 
 Desenvolvimento de uma landing page funcional destinada à apresentação de um portfólio pessoal. Trabalho realizado como atividade prática extra do curso de Introdução à Programação Front e Back-End apresentada pelo programa SCTEC, parceria entre o SENAI e o SCTI. A página foi desenvolvida utilizando as linguagens HTML, CSS e JavaScript.
 
-A página contém três arquivos principais, uma em cada linguagem mencionada: index.html, styles.css e script.js. Uma sub-pasta chamada pictures contém todas as imagens utilizadas para o desenvolvimento da página. É importante que todos estes documentos estejam reunidos e estruturados conforme disponibilizada ou apresentada no repositório.
+A página contém quatro arquivos principais, uma em cada linguagem mencionada: index.html, styles.css e script.js, além do arquivo translations.json que armazena os textos das versões em Português e em Inglês. Uma sub-pasta chamada pictures contém todas as imagens utilizadas para o desenvolvimento da página. É importante que todos estes documentos estejam reunidos e estruturados conforme disponibilizada ou apresentada no repositório.
 
 A página desenvolvida apresenta infomações do autor, inicialmente mencionando o propósito principal da página com intuito de conquistar o interesse de algum recrutador ou contratante para serviços especializados de desenvolvimento tecnológico, através de textos e figuras. A página foi construída de maneira responsiva, permitindo a sua leitura em diferentes tamanhos de tela.
 
-O cabeçalho apresenta uma logomarca e os nomes das seções contidas na página, com atalhos diretos às mesmas através do clique ou toque na seção desejada. Em resolução reduzida, as opções ficam concentradas sob um botão de menu hamburguer.
+O cabeçalho apresenta uma logomarca e os nomes das seções contidas na página, com atalhos diretos às mesmas através do clique ou toque na seção desejada. Em resolução reduzida, as opções ficam concentradas sob um botão de menu hamburguer. Além disso, possui um botão onde é possível trocar o idioma da página entre as versões em Português ou Inglês.
 
 Na sequencia, a página apresenta o nome do profissional e aluno do curso mencionado acima, junto a sua descrição pessoal focada em seus objetivos profissionais.
 
-A seção seguinte de Projetos, apresentam três aplicações recentemente desenvolvidas pelo autor. Duas delas como exercícios desenvolvidos durante o curso e uma delas um projeto de sistema eletrônico desenvolvido pelo autor. Todos os arquivos relacionados estão disponíveis no repositório GitHub do autor com link direto aos projetos, assim como uma cópia desta página desenvolvida.
+A seção seguinte de Serviços, apresentam as principais atividades e serviços oferecidos pelo autor à outras empresas ou contratos de projetos de trabalho freelance, de acordo com a sua experiência ao longo da carreira.
 
-A seção de Habilidades vem na sequencia, apresentando as cinco principais habilidades de cada área proposta pelo autor: Linguagens de Programação, Frameworks, Ferramentas, Competências e Soft-Skills. 
+A seção de Projetos apresenta três aplicações desenvolvidas pelo autor. Duas delas como produtos desenvolvidos em colaboração com empresas em contratos recentes, e uma delas um projeto de sistema eletrônico desenvolvido pelo autor. Os links dos produtos relacionados estão disponíveis na página, e os arquivos dos projetos relacionados estão disponíveis no repositório GitHub do autor com link direto aos projetos, assim como uma cópia desta página desenvolvida.
+
+A seção de Habilidades vem na sequencia, apresentando as cinco principais habilidades de cada área proposta pelo autor: Linguagens de Programação, Ferramentas, Soft-Skills, Técnicas de layout de PCBs e de CIs. Outras categorias foram omissas para não ficar redundante com as atividades descritas na seção de Serviços. 
 
 A seção de Contatos apresentam as informações de E-mail, Telefone, Localidade, Atalhos das redes sociais LinkedIn e GitHub, todas contendo links direto codificados através de comandos JavaScript.
 
-O rodapé da página encerra o seu desenvolvimento, informando a data que a página foi desenvolvida e o seu autor.
+O rodapé da página encerra o seu desenvolvimento, informando o ano que a página foi desenvolvida e o seu autor.
--- script.js
+++ script.js
@@ -1,6 +1,181 @@
-let button = document.getElementById("hamburger");
+let currLang = "ptbr";
+let lang = document.getElementById("lang");
+
+async function changeLanguage(opt) {
+  const response = await fetch("translations.json");
+  const data = await response.json();
+  document.getElementById("menu1").textContent = data[opt].txt_menu1;
+  document.getElementById("menu2").textContent = data[opt].txt_menu2;
+  document.getElementById("menu3").textContent = data[opt].txt_menu3;
+  document.getElementById("menu4").textContent = data[opt].txt_menu4;
+  document.getElementById("menu5").textContent = data[opt].txt_menu5;
+  document.getElementById("home-title").innerHTML = data[opt].txt_home_title;
+  document.getElementById("about1").textContent = data[opt].txt_about1;
+  document.getElementById("about2").textContent = data[opt].txt_about2;
+  document.getElementById("about3").textContent = data[opt].txt_about3;
+  document.getElementById("about4").textContent = data[opt].txt_about4;
+  document.getElementById("services-title").textContent =
+    data[opt].txt_services_title;
+  document.getElementById("services-description").textContent =
+    data[opt].txt_services_description;
+  document.getElementById("serviceSW-title").textContent =
+    data[opt].txt_serviceSW_title;
+  document.getElementById("serviceSW-description").textContent =
+    data[opt].txt_serviceSW_description;
+  document.getElementById("serviceSE-title").textContent =
+    data[opt].txt_serviceSE_title;
+  document.getElementById("serviceSE-description").textContent =
+    data[opt].txt_serviceSE_description;
+  document.getElementById("serviceCI-title").textContent =
+    data[opt].txt_serviceCI_title;
+  document.getElementById("serviceCI-description").textContent =
+    data[opt].txt_serviceCI_description;
+  document.getElementById("serviceEA-title").textContent =
+    data[opt].txt_serviceEA_title;
+  document.getElementById("serviceEA-description").textContent =
+    data[opt].txt_serviceEA_description;
+  document.getElementById("serviceHW-title").textContent =
+    data[opt].txt_serviceHW_title;
+  document.getElementById("serviceHW-description").textContent =
+    data[opt].txt_serviceHW_description;
+  document.getElementById("serviceET-title").textContent =
+    data[opt].txt_serviceET_title;
+  document.getElementById("serviceET-description").textContent =
+    data[opt].txt_serviceET_description;
+  document.getElementById("servicePE-title").textContent =
+    data[opt].txt_servicePE_title;
+  document.getElementById("servicePE-description").textContent =
+    data[opt].txt_servicePE_description;
+  document.getElementById("projects-title").textContent =
+    data[opt].txt_projects_title;
+  document.getElementById("projects-description").textContent =
+    data[opt].txt_projects_description;
+  document.getElementById("projectBC-title").textContent =
+    data[opt].txt_projectBC_title;
+  document.getElementById("projectBC-description").textContent =
+    data[opt].txt_projectBC_description;
+  document.getElementById("projectBC-link").textContent =
+    data[opt].txt_projectBC_link;
+  document.getElementById("projectOV-title").textContent =
+    data[opt].txt_projectOV_title;
+  document.getElementById("projectOV-description").textContent =
+    data[opt].txt_projectOV_description;
+  document.getElementById("projectOV-link").textContent =
+    data[opt].txt_projectOV_link;
+  document.getElementById("projectRP-title").textContent =
+    data[opt].txt_projectRP_title;
+  document.getElementById("projectRP-description").textContent =
+    data[opt].txt_projectRP_description;
+  document.getElementById("projectRP-link").textContent =
+    data[opt].txt_projectRP_link;
+  document.getElementById("skills-title").textContent =
+    data[opt].txt_skills_title;
+  document.getElementById("skills-description").textContent =
+    data[opt].txt_skills_description;
+  document.getElementById("skillPL-title").textContent =
+    data[opt].txt_skillPL_title;
+  document.getElementById("skillPL-item1").textContent =
+    data[opt].txt_skillPL_item1;
+  document.getElementById("skillPL-item2").textContent =
+    data[opt].txt_skillPL_item2;
+  document.getElementById("skillPL-item3").textContent =
+    data[opt].txt_skillPL_item3;
+  document.getElementById("skillPL-item4").textContent =
+    data[opt].txt_skillPL_item4;
+  document.getElementById("skillPL-item5").textContent =
+    data[opt].txt_skillPL_item5;
+  document.getElementById("skillFW-title").textContent =
+    data[opt].txt_skillFW_title;
+  document.getElementById("skillFW-item1").textContent =
+    data[opt].txt_skillFW_item1;
+  document.getElementById("skillFW-item2").textContent =
+    data[opt].txt_skillFW_item2;
+  document.getElementById("skillFW-item3").textContent =
+    data[opt].txt_skillFW_item3;
+  document.getElementById("skillFW-item4").textContent =
+    data[opt].txt_skillFW_item4;
+  document.getElementById("skillFW-item5").textContent =
+    data[opt].txt_skillFW_item5;
+  document.getElementById("skillTO-title").textContent =
+    data[opt].txt_skillTO_title;
+  document.getElementById("skillTO-item1").textContent =
+    data[opt].txt_skillTO_item1;
+  document.getElementById("skillTO-item2").textContent =
+    data[opt].txt_skillTO_item2;
+  document.getElementById("skillTO-item3").textContent =
+    data[opt].txt_skillTO_item3;
+  document.getElementById("skillTO-item4").textContent =
+    data[opt].txt_skillTO_item4;
+  document.getElementById("skillTO-item5").textContent =
+    data[opt].txt_skillTO_item5;
+  document.getElementById("skillSS-title").textContent =
+    data[opt].txt_skillSS_title;
+  document.getElementById("skillSS-item1").textContent =
+    data[opt].txt_skillSS_item1;
+  document.getElementById("skillSS-item2").textContent =
+    data[opt].txt_skillSS_item2;
+  document.getElementById("skillSS-item3").textContent =
+    data[opt].txt_skillSS_item3;
+  document.getElementById("skillSS-item4").textContent =
+    data[opt].txt_skillSS_item4;
+  document.getElementById("skillSS-item5").textContent =
+    data[opt].txt_skillSS_item5;
+  document.getElementById("skillPCB-title").textContent =
+    data[opt].txt_skillPCB_title;
+  document.getElementById("skillPCB-item1").textContent =
+    data[opt].txt_skillPCB_item1;
+  document.getElementById("skillPCB-item2").textContent =
+    data[opt].txt_skillPCB_item2;
+  document.getElementById("skillPCB-item3").textContent =
+    data[opt].txt_skillPCB_item3;
+  document.getElementById("skillPCB-item4").textContent =
+    data[opt].txt_skillPCB_item4;
+  document.getElementById("skillPCB-item5").textContent =
+    data[opt].txt_skillPCB_item5;
+  document.getElementById("skillIC-title").textContent =
+    data[opt].txt_skillIC_title;
+  document.getElementById("skillIC-item1").textContent =
+    data[opt].txt_skillIC_item1;
+  document.getElementById("skillIC-item2").textContent =
+    data[opt].txt_skillIC_item2;
+  document.getElementById("skillIC-item3").textContent =
+    data[opt].txt_skillIC_item3;
+  document.getElementById("skillIC-item4").textContent =
+    data[opt].txt_skillIC_item4;
+  document.getElementById("skillIC-item5").textContent =
+    data[opt].txt_skillIC_item5;
+  document.getElementById("contacts-title").textContent =
+    data[opt].txt_contacts_title;
+  document.getElementById("contacts-description").textContent =
+    data[opt].txt_contacts_description;
+  document.getElementById("contact1-name").textContent =
+    data[opt].txt_contact1_name;
+  document.getElementById("contact1-value").textContent =
+    data[opt].txt_contact1_value;
+  document.getElementById("contact2-name").textContent =
+    data[opt].txt_contact2_name;
+  document.getElementById("contact2-value").textContent =
+    data[opt].txt_contact2_value;
+  document.getElementById("contact3-name").textContent =
+    data[opt].txt_contact3_name;
+  document.getElementById("contact3-value").textContent =
+    data[opt].txt_contact3_value;
+}
+
+function toggleLanguage() {
+  if (currLang === "ptbr") {
+    currLang = "enus";
+    lang.innerHTML = '<img src="./pictures/us_flag.png" height="20">';
+  } else {
+    currLang = "ptbr";
+    lang.innerHTML = '<img src="./pictures/br_flag.png" height="20">';
+  }
+  changeLanguage(currLang);
+}
+
+let hamb = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
-button.addEventListener("click", function () {
+hamb.addEventListener("click", function () {
   menu.classList.toggle("ativa");
 });
 
